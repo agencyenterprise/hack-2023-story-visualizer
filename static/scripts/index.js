99df9d337abb488b5d0c6bc51aa9adee34c43afd
EgonStep <egonstep@hotmail.com>
2023-09-27 18:04:45 -0300

    text added

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const toggleButton = document.getElementById("toggle");
+const toogleText = document.getElementById("toggle-text");
 const languageSelect = document.getElementById("language");
 const progressBar = document.getElementById("progress");
 
@@ -85,6 +86,7 @@ const stopRecognition = () => {
     recognition.stop();
     recognition = null;
     toggleButton.classList.remove("animate-bounce");
+    toogleText.textContent = "Try to use short sentences (OFF)";
   }
 };
 
@@ -118,6 +120,7 @@ const startRecognition = () => {
 
     // Animation
     toggleButton.classList.add("animate-bounce");
+    toogleText.textContent = "Try to use short sentences (ON)";
   }
 };
 
