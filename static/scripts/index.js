ee37a02335a3f3fc39a938e9cf6dc851ff678d52
EgonStep <egonstep@hotmail.com>
2023-09-27 15:54:19 -0300

    text param to skeleton method

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -2,47 +2,48 @@
 
 const toggleButton = document.getElementById("toggle");
 const languageSelect = document.getElementById("language");
+const progressBar = document.getElementById("progress");
 
 function reverseChildren(parent) {
-  for (var i = 1; i < parent.childNodes.length; i++){
-      parent.insertBefore(parent.childNodes[i], parent.firstChild);
+  for (var i = 1; i < parent.childNodes.length; i++) {
+    parent.insertBefore(parent.childNodes[i], parent.firstChild);
   }
 }
 
-function appendSkeleton() {
-    // Create main skeleton div
-    const existingSkeleton = document.getElementById('skeleton');
-    if (existingSkeleton) return;
+function appendSkeleton(text) {
+  // Create main skeleton div
+  const existingSkeleton = document.getElementById("skeleton");
+  if (existingSkeleton) return;
 
-    const skeleton = document.createElement('div');
-    skeleton.id = 'skeleton';
-    skeleton.className = 'w-full sm:w-[45%]';
+  const skeleton = document.createElement("div");
+  skeleton.id = "skeleton";
+  skeleton.className = "w-full sm:w-[45%]";
 
-    // Create inner div
-    const innerDiv = document.createElement('div');
-    innerDiv.className = 'border border-gray-300 rounded-lg p-4 bg-gray-200 flex flex-col items-center gap-2 animate-pulse w-full';
+  // Create inner div
+  const innerDiv = document.createElement("div");
+  innerDiv.className =
+    "border border-gray-300 rounded-lg p-4 bg-gray-200 flex flex-col items-center gap-2 animate-pulse w-full";
 
-    // Create 3 child divs and add them to the innerDiv
-    for (let i = 0; i < 3; i++) {
-        const childDiv = document.createElement('div');
-        if (i === 0) {
-            childDiv.className = 'aspect-square w-full max-w-[1000px] max-h-[1000px] bg-gray-300 rounded';
-        } else {
-            childDiv.className = 'h-4 bg-gray-300 rounded' + ((i === 2) ? ' w-1/3' : ' w-full');
-        }
-        innerDiv.appendChild(childDiv);
-    }
+  const childDiv = document.createElement("div");
+  childDiv.className =
+    "aspect-square w-full max-w-[1000px] max-h-[1000px] bg-gray-300 rounded";
+  innerDiv.appendChild(childDiv);
 
-    // Add innerDiv to skeleton
-    skeleton.appendChild(innerDiv);
-    // Append skeleton to the body or another container
-    const imagesContainer = document.getElementById("images")
-    imagesContainer.prepend(skeleton);
+  const pTag = document.createElement("p");
+  pTag.className = "text-sm";
+  pTag.innerText = text;
+  innerDiv.appendChild(pTag);
+
+  // Add innerDiv to skeleton
+  skeleton.appendChild(innerDiv);
+  // Append skeleton to the body or another container
+  const imagesContainer = document.getElementById("images");
+  imagesContainer.prepend(skeleton);
 }
 
 function removeSkeleton() {
-    const skeleton = document.getElementById('skeleton');
-    if (skeleton) skeleton.remove();
+  const skeleton = document.getElementById("skeleton");
+  if (skeleton) skeleton.remove();
 }
 
 let recognition = null;
@@ -62,16 +63,26 @@ const toggleListening = () => {
     // recognition.lang = 'pt-BR'     recognition.start();
     // recognition.lang = 'pt-BR'
     recognition.start();
+    recognition.addEventListener("onnomatch", handleNoMatch);
+    recognition.addEventListener("error", handleError);
     recognition.addEventListener("result", convertToText);
     toggleButton.classList.add("animate-bounce");
   }
 };
 
+const handleError = async (event) => {
+  console.log(event);
+};
+
+const handleNoMatch = async (event) => {
+  console.log(event);
+};
+
 const convertToText = async (event) => {
   for (let i = event.resultIndex; i < event.results.length; i++) {
     if (event.results[i].isFinal) {
-      appendSkeleton();
       const content = event.results[i][0].transcript.trim();
+      appendSkeleton(content);
       await fetchAndDisplayImage(content); // Call the function with the final text
     }
   }
--- static/scripts/requests.js
+++ static/scripts/requests.js
@@ -1,58 +1,59 @@
 "use strict";
 
 async function fetchAndDisplayImage(text) {
-    appendSkeleton();
+  const response = await fetch("/generate_image", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text: text }),
+  });
+  const data = await response.json();
+  //    document.querySelector(".lds-facebook").style = "display:none";
 
-    const response = await fetch("/generate_image", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ text: text }),
-    });
-    const data = await response.json();
-//    document.querySelector(".lds-facebook").style = "display:none";
+  if (data.image) {
+    // Create main div
+    const mainDiv = document.createElement("div");
+    mainDiv.className =
+      "border border-gray-300 rounded-lg p-4 bg-gray-200 flex flex-col items-center gap-2 w-full sm:w-[45%]";
 
-    if (data.image) {
-      // Create main div
-      const mainDiv = document.createElement('div');
-      mainDiv.className = 'border border-gray-300 rounded-lg p-4 bg-gray-200 flex flex-col items-center gap-2 w-full sm:w-[45%]';
+    // Create container div and img
+    const containerDiv = document.createElement("div");
+    containerDiv.className =
+      "aspect-square w-full max-w-[1000px] max-h-[1000px]";
+    containerDiv.style.background =
+      "url(/static/img-loader.svg) no-repeat center";
 
-      // Create container div and img
-      const containerDiv = document.createElement('div');
-      containerDiv.className = 'aspect-square w-full max-w-[1000px] max-h-[1000px]';
-      containerDiv.style.background = "url(/static/img-loader.svg) no-repeat center";
+    const img = document.createElement("img");
+    img.src = data.image;
+    img.alt = "Instagram Post";
+    img.className = "aspect-square w-full h-full max-w-[1000px] max-h-[1000px]";
+    img.style.opacity = "0";
+    img.style.transition = "all 0.2s ease";
+    img.onerror = "this.onerror=null; this.src='/static/img-loader.svg'";
+    //      img.onload = "this.style.opacity=1;";
 
-      const img = document.createElement('img');
-      img.src = data.image;
-      img.alt = 'Instagram Post';
-      img.className = 'aspect-square w-full h-full max-w-[1000px] max-h-[1000px]';
-      img.style.opacity = '0';
-      img.style.transition = 'all 0.2s ease';
-      img.onerror = "this.onerror=null; this.src='/static/img-loader.svg'";
-//      img.onload = "this.style.opacity=1;";
+    // Add img to containerDiv
+    containerDiv.appendChild(img);
 
-      // Add img to containerDiv
-      containerDiv.appendChild(img);
+    // Create p
+    const pTag = document.createElement("p");
+    pTag.className = "text-sm";
+    pTag.innerText = text;
 
-      // Create p
-      const pTag = document.createElement('p');
-      pTag.className = 'text-sm';
-      pTag.innerText = text;
+    // Add containerDiv and pTag to mainDiv
+    mainDiv.appendChild(containerDiv);
+    mainDiv.appendChild(pTag);
 
-      // Add containerDiv and pTag to mainDiv
-      mainDiv.appendChild(containerDiv);
-      mainDiv.appendChild(pTag);
+    // Add mainDiv to the images container
+    removeSkeleton();
+    const imagesContainer = document.getElementById("images");
+    imagesContainer.prepend(mainDiv);
 
-      // Add mainDiv to the images container
-      removeSkeleton();
-      const imagesContainer = document.getElementById("images")
-      imagesContainer.prepend(mainDiv);
-
-      // window.scrollTo(0,document.body.scrollHeight);
-      img.onload = function () {
-        img.scrollIntoView({ behavior: "smooth", block: "end" }); // Scroll to the last image added
-        img.style.opacity = "1";
-      };
-    }
+    // window.scrollTo(0,document.body.scrollHeight);
+    img.onload = function () {
+      img.scrollIntoView({ behavior: "smooth", block: "end" }); // Scroll to the last image added
+      img.style.opacity = "1";
+    };
+  }
 }
