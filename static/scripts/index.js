2a6205414e9c4ce416fff344f4071d1d5e2d97cc
leandrovi <leandrofernandesvieira@gmail.com>
2023-09-27 18:22:46 -0300

    fix: reduce text size

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -33,7 +33,7 @@ function appendSkeleton(text, id) {
   innerDiv.appendChild(childDiv);
 
   const pTag = document.createElement("p");
-  pTag.className = "text-sm lg:text-base xl:text-3xl text-center p-4";
+  pTag.className = "text-sm lg:text-base xl:text-lg text-center p-4";
   pTag.innerText = text;
   innerDiv.appendChild(pTag);
 
