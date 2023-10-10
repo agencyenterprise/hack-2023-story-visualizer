e5680f8433a56dccec1784ed8a9dc1af754cf7cf
EgonStep <egonstep@hotmail.com>
2023-10-10 15:17:52 -0300

    added sentry

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "@typeform/embed/build/css/popup.css";
 import "./globals.css";
 
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -37,7 +38,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Script
+          src="https://scripts.simpleanalyticscdn.com/latest.js"
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   );
 }
